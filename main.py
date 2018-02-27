3f5c2930982013772788db4252c851e4509942eb
276947 przywarty adam <przywartya@p21815.mini.pw.edu.pl>
2018-02-27 13:36:01 +0100

    Add alchemists guilds.

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ NUMBER_OF_WARLOCKS = 3
 NUMBER_OF_SORCERERS = 3
 FACTORY_CAPACITY = 2
 PRODUCTION_WAIT = 0.5
+WARLOCK_WAIT = 1
+SORCERER_WAIT = 0.7
 
 
 class Alchemist:
@@ -13,14 +15,25 @@ class Alchemist:
 
 
 class Warlock:
-    def cast_curse(self, factory):
+    def run(self, factory):
+        while True:
+            self.cast_curse(factory)
+
+    @staticmethod
+    def cast_curse(factory):
+        time.sleep(WARLOCK_WAIT)
         factory.curses += 1
 
 
 class Sorcerer:
-    def remove_curse(self, factory):
-        if factory.curses > 0:
-            factory.curses -= 1
+    def run(self, factory):
+        while True:
+            self.remove_curse(factory)
+
+    @staticmethod
+    def remove_curse(factory):
+        time.sleep(SORCERER_WAIT)
+        factory.curses -= 1
 
 
 class Resource:
@@ -54,6 +67,22 @@ if __name__ == "__main__":
     lead = Resource(rtype=Resource.TYPES['lead'])
     sulfur = Resource(rtype=Resource.TYPES['sulfur'])
     mercury = Resource(rtype=Resource.TYPES['mercury'])
+    GUILD_A = {
+        'resources': [lead, mercury],
+        'alchemists': []
+    }
+    GUILD_B = {
+        'resources': [mercury, sulfur],
+        'alchemists': []
+    }
+    GUILD_C = {
+        'resources': [lead, sulfur],
+        'alchemists': []
+    }
+    GUILD_D = {
+        'resources': [mercury, sulfur, lead],
+        'alchemists': []
+    }
     Factory(resources=[lead], curses=0)
     Factory(resources=[sulfur], curses=0)
     Factory(resources=[mercury], curses=0)
